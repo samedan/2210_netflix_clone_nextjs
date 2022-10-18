763b35f73005e67016a6cc38737c2263bc015984
Daniel Popescu <same.dan@gmail.com>
2022-10-18 10:33:33 +0200

    Teletravail

--- pages/_document.js
+++ pages/_document.js
@@ -10,9 +10,10 @@ export default function Document() {
         href="https://fonts.googleapis.com"
         crossOrigin="true"
       />
+
       <link
+        href="https://fonts.googleapis.com/css2?family=Roboto+Slab:wght@700&display=swap"
         rel="stylesheet"
-        href="https://fonts.googleapis.com/css2?family=Roboto+Slab&display=swap"
       />
       <body>
         <Main />
--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,28 @@
+import Head from "next/head";
 import styles from "../styles/Home.module.css";
+
+import Banner from "../components/banner/banner";
+import NavBar from "../components/nav/navbar";
+import Card from "../components/card/card";
 
 export default function Home() {
   return (
     <div className={styles.container}>
-      <title>Netflix clone</title>
+      <Head>
+        <title>Netflix</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
 
-      <h1 className={styles.title}>Netflix</h1>
+      <NavBar username="ankita@ank.com" />
+      <Banner
+        title="Clifford the red dog"
+        subTitle="a very cute dog"
+        imgUrl="/static/clifford.webp"
+      />
+
+      <Card imgUrl="/static/clifford.webp" size="large" />
+      <Card size="medium" />
+      <Card imgUrl="/static/clifford.webp" size="small" />
     </div>
   );
 }
